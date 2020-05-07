8c8422dbce28e4bb937fbc2db05c385381fa32b6
Toni <megaman7de@googlemail.com>
2020-05-07 17:51:39 +0200

    strukturelle verbesserungen

--- js/board.js
+++ js/board.js
@@ -1,67 +1,66 @@
 // Überführt eine Tabelle in ein KartenSchachBrett
 // hidden gibt an wie groß die "SpringerWelt" ist; diese Felder sind dann zunächst ausgeblendet
 // @return: DOM-Tree Tabelle mit Attributen
-function createBoard(table, hidden)
+function createBoard(hidden)
 {
-	if(table.rows.length>=8 && table.rows.length%2==0)
-	{
-        //richtige größe finden
-        var min=Math.min(window.innerWidth,window.innerHeight);
-        var size=Math.floor(min*0.9/10)*10;
-        
-        table.setAttribute("id","table1");
-        table.setAttribute("width",size);
-        table.setAttribute("height",size);
-        
-        // richtige startwerte für ids
-        var start_x="a".charCodeAt(0)-hidden;
-        var start_y=1-hidden;
-        
-        var trs=table.getElementsByTagName("tr");
-        var table_size=trs.length-1;
-        // zeilen durchgehen
-        for(var i=0;i<trs.length;i++)
+	// Grundtabelle erstellen
+	var table=createTable(8+2*hidden);
+
+    //richtige größe finden
+    var min=Math.min(window.innerWidth,window.innerHeight);
+    var size=Math.floor(min*0.9/10)*10;
+    
+    table.setAttribute("id","table1");
+    table.setAttribute("width",size);
+    table.setAttribute("height",size);
+    
+    // richtige startwerte für ids
+    var start_x="a".charCodeAt(0)-hidden;
+    var start_y=1-hidden;
+    
+    var trs=table.getElementsByTagName("tr");
+    var table_size=trs.length-1;
+    // zeilen durchgehen
+    for(var i=0;i<trs.length;i++)
+    {
+        var tds=trs[i].getElementsByTagName("td");
+        // spalten durch gehen
+        for(var j=0;j<tds.length;j++)
         {
-        	var tds=trs[i].getElementsByTagName("td");
-        	// spalten durch gehen
-        	for(var j=0;j<tds.length;j++)
-        	{
-        		var td=tds[j];
-        		
-        		/* ids festlegen
-        		8x8, hidden=0: links unten=a1
-        		10x10, hidden=1: links unten=`0
-        		12x12. hidden=2: links unten=_-1
-        		
-        		charCode Tabelle:
-        		"95": "_", "96": "`",     "97": "a",     "98": "b"
-        		*/
-        		var x=String.fromCharCode(start_x+j);
-        		var y=start_y+(table_size-i);
-        		td.setAttribute("id",x+""+y);
-        		
-        		// "weiß"=#ffce9e; "schwarz"=#d18b47
-        		if((x.charCodeAt(0)+y)%2==0)
-        		{
-        			td.setAttribute("style","background-color:#d18b47");
-        		}
-        		else
-        		{
-        			td.setAttribute("style","background-color:#ffce9e");
-        		}
-        		
-        		// unsichtbar setzen
-        		if(x.charCodeAt(0)>104 || x.charCodeAt(0)<97 || y<1 || y>8)
-        		{
-        			td.setAttribute("style","visibility:hidden");
-        		}
-        		
-        		// javascript onClick vorbereiten
-        		td.setAttribute("onClick","javascript:someFunction(\"" + td.getAttribute("id") + "\");");
-        		
-        	}
+            var td=tds[j];
+            
+            /* 	ids festlegen
+                8x8, hidden=0: links unten=a1
+                10x10, hidden=1: links unten=`0
+                12x12. hidden=2: links unten=_-1
+                
+                charCode Tabelle:
+                "95": "_", "96": "`", "97": "a", "98": "b"
+            */
+            var x=String.fromCharCode(start_x+j);
+            var y=start_y+(table_size-i);
+            td.setAttribute("id",x+""+y);
+            
+            // "weiß"=#ffce9e; "schwarz"=#d18b47
+            if((x.charCodeAt(0)+y)%2==0)
+            {
+                td.style.backgroundColor="#d18b47";
+            }
+            else
+            {
+                 td.style.backgroundColor="#ffce9e";
+            }
+            
+            // unsichtbar setzen
+            if(x.charCodeAt(0)>104 || x.charCodeAt(0)<97 || y<1 || y>8)
+            {
+                td.style.visibility="hidden";
+            }
+            
+            // javascript onClick vorbereiten
+            td.setAttribute("onClick","javascript:someFunction(\"" + td.getAttribute("id") + "\");");           
         }
-	}
+    }
 	return table;
 }
 
