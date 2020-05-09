6f2f56f29953af795cbd9b9ae88c300fc99a0417
[Antonio] <[antonio.rajic@student.uni-zuebingen.de]>
2020-05-09 02:59:07 +0200

    Figur-Objekt vollständig

--- js/feld.js
+++ js/feld.js
@@ -23,12 +23,16 @@ function feld(_id, _figur)
 	this.setCards=function(cardId)
 	{
 		cards.push(cardId);
-		// toDo: bild dem feld hinzufügen
+		// toDo: bild dem feld hinzufügen (eigentlich view aufgabe)
 		// welches bild wenn 2 Karten auf ein feld gespielt wurden?
 	}
 	this.setFigur=function(fi)
-	{
-		figur.push(fi);
+    {
+        // die figur muss das feld (feldId) erben, weil child nicht auf parent zugreifen kann
+        // todo Setter feldId schreiben
+        fi.setFeldId(this.id);
+        figur.push(fi);
+
 		if(figur.length>0)
 		{
 			this.empty=false;
@@ -59,20 +63,81 @@ function feld(_id, _figur)
 	}
 	
 	//falls parameter übergeben worden sind
-	if(_id)this.setId(_id);
-	if(_figur)this.setFigur(_figur);
+    if (_id) {
+        this.setId(_id);
+    }
+    if (_figur) {
+        this.setFigur(_figur);
+    }
 }
 
 // definiert das figur-Objekt
-function figur(name)
+function figur(_name)
 {
+    var feldId = "";
 	var id = "";
 	var img = "";
-	var name = "";
+    var name = "";
+    var type = "";
 	var color = "";
 	var moves = [];
 	var cards = [];
 
+    // getters
+    this.id = id;
+    this.img = img;
+    this.name = name;
+    this.type = type;
+    this.color = color;
+    this.feldId = feldId;
+    this.moves = moves;
+    this.cards = cards;
+
+    // setters
+    this.setFeldId = function (feldId) {
+        this.feldId = feldId;
+    }
+    this.setName = function (name) {
+        this.name = name;
+        this.type = name.substring(0, 1);
+        this.color = name.substring(1, 2);
+        var prefix = "img/svg/";
+        var suffix = ".svg";
+        this.img = prefix + name + suffix;
+        // figuren id nötig weil 2 figuren auf einem feld stehen können?!
+        this.id = Math.random().toString(36).substr(2, 9);
+    }
+    // toDo zuggenerator/zuglogic fehlt
+    this.setMoves = function () {
+        // ganze figur wird übergeben
+        this.moves = someMoveFunction(this);
+    }
+
+    this.setCards = function (cardId) {
+        cards.push(cardId);
+    }
+
+    // functions
+    this.removeCard = function (cardId) {
+        removeE(cards, cardId);
+    }
+
+    // falls mit parameter aufgerufen wird
+    if (_name) {
+        this.setName(_name);
+    }
 }
 
-// visual studio test
+//leere funktion für züge damit keine fehler kommen
+function someMoveFunction(figur) {
+    console.log(figur.name + " steht auf " + figur.feldId);
+
+    return ["e2-e3", "e2-e4"];
+}
+
+// tests
+var fig = new figur("Rw");
+fig.setFeldId("a1");
+fig.setMoves();
+var m = fig.moves;
+console.log(m);
