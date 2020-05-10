7534d2a5010e242bb84b66d037d7626079f8e002
[Antonio] <[antonio.rajic@student.uni-zuebingen.de]>
2020-05-11 01:01:13 +0200

    Card-Objekt erstellt

---
+++ js/cards.js
@@ -0,0 +1,222 @@
+var alleKarten=[
+  {
+    "cardId": 1,
+    "title_de": "Ritterwelt",
+    "title_en": "Kinghtsworld",
+    "type": "Weltkarte",
+    "text": "Es existeriert eine Rahmenfeld um das gesamte Spiel, dieses darf nur von Springern genutzt werden. Diese Karte setzt die Zylinderwelt für Springer außer Kraft."
+  },
+  {
+    "cardId": 2,
+    "title_de": "Bipolarität",
+    "title_en": "Sexchange",
+    "type": "Zugkarte",
+    "text": "Du darfst einen eigenen Läufer um ein Feld nach rechts versetzen, wenn es frei ist."
+  },
+  {
+    "cardId": 3,
+    "title_de": "Bauernvormarsch",
+    "title_en": "On The Rise",
+    "type": "Zugkarte",
+    "text": "Die Bauern schlagen im nächsten Zug gerade und fahren diagonal"
+  },
+  {
+    "cardId": 4,
+    "title_de": "Trojanisches Pferd",
+    "title_en": "Trojan Horse",
+    "type": "Zugkarte",
+    "text": "Ein eigener Turm wird zu einem Pferd."
+  },
+  {
+    "cardId": 5,
+    "title_de": "Neubevölkerung",
+    "title_en": "tenacious infantry",
+    "type": "Zugkarte",
+    "text": "Du darfst einen Bauern an einer freien Stelle, in deiner Bauernstartreihe wiederbeleben."
+  },
+  {
+    "cardId": 6,
+    "title_de": "Versteckte Liebschaft",
+    "title_en": "Polygamy of the king",
+    "type": "Zugkarte",
+    "text": "Opfere beide Türm, du erhälst eine Dame an der Position deiner Türm, der Gegner entscheidet an welcher."
+  },
+  {
+    "cardId": 7,
+    "title_de": "Geheimgänge",
+    "title_en": "secret passage",
+    "type": "Zugkarte",
+    "text": "Dein König hat im nächsten Zug Radius 2 und darf hüpfen."
+  },
+  {
+    "cardId": 8,
+    "title_de": "Heiliger Krieg",
+    "title_en": "holy war",
+    "type": "Zugkarte",
+    "text": "Die Läufer fahren einen Zug wie Türme."
+  },
+  {
+    "cardId": 9,
+    "title_de": "Bürgeropfer",
+    "title_en": "Sacrifice for the greater good",
+    "type": "Zugkarte",
+    "text": "Opfere eine deiner Kreatur, zerstöre eine schwächer Kreatur deines Gegners (Reihenfolge: Dame, Turm , Läufer und Pferd gleichstark, Bauer)"
+  },
+  {
+    "cardId": 10,
+    "title_de": "Erzwungener Frieden",
+    "title_en": "enforced peace treaty",
+    "type": "Zugkarte",
+    "text": "Im den nächsten 2 Zügen können keine Figuren geschlagen werden. (Jedoch dürfen, wenn andere Karten dies erlauben zerstört oder geopfert werden.)"
+  },
+  {
+    "cardId": 11,
+    "title_de": "Fallenfeld",
+    "title_en": "Trap",
+    "type": "Weltkarte",
+    "text": "Bestimme ein Feld, jede Figur die auf dieses Feld zieht wird zerstört. (Die Figur wird nicht zerstört, wenn die Figur dort schon steht oder auf dem Feld eingesetzt wird.)"
+  },
+  {
+    "cardId": 12,
+    "title_de": "Blockade",
+    "title_en": "blockade",
+    "type": "Weltkarte",
+    "text": "Setze eine Blockade auf ein freies Feld, diese wird behandelt wie eine Figur, die nicht geschlagen werden kann (blockiert das Portal sowie das Fallenfeld)."
+  },
+  {
+    "cardId": 13,
+    "title_de": "Zusammenarbeit",
+    "title_en": "Teamwork",
+    "type": "Zugkarte",
+    "text": "Tausche eine Figuren mit einer Wertigkeit von 3 mit einem Turm. Dein Gegner hat die Wahl ob er auch tauscht oder einen Zug macht."
+  },
+  {
+    "cardId": 14,
+    "title_de": "Doppelgänger",
+    "title_en": "Double",
+    "type": "Zugkarte",
+    "text": "Vertausche deinen König mit einem Bauern."
+  },
+  {
+    "cardId": 15,
+    "title_de": "Zweite Chance",
+    "title_en": "Second Chance",
+    "type": "Zugkarte",
+    "text": "Der letzte Zug wird rückgängig gemacht und man darf seinen Halbzug ändern. Dies darf nur ausgespielt werden, wenn im letzten Zug keine Karten gespielt wurden."
+  },
+  {
+    "cardId": 16,
+    "title_de": "Rückzug",
+    "title_en": "retreat",
+    "type": "Zugkarte",
+    "text": "Die Bauern fahren im nächsten Zug rückwärts, das heißt genau so als hätten sie von der anderen Seite gestartet."
+  },
+  {
+    "cardId": 17,
+    "title_de": "Zylinderwelt",
+    "title_en": "Cylindric-world",
+    "type": "Weltkarte",
+    "text": "Alle Figuren dürfen die Rechte und Linke Grenze des Standartypielfeldes überschreiten als ob sie mit einander verbunden wären und auf der gegenüberligenden Seite ihre Bewegung fortfahren. Diese Karte darf nicht gespielt werden, wenn der gegnerische König dadurch bedroht wird."
+  },
+  {
+    "cardId": 18,
+    "title_de": "Koalition",
+    "title_en": "coalition",
+    "type": "Zugkarte",
+    "text": "Im nächsten Zug darf man eine Figur auf ein Feld ziehen auf dem schon eine eigene Figur steht. Dies muss nicht aufgelöst werden, jedoch können beide wie eine Figur geschlagen werden."
+  },
+  {
+    "cardId": 19,
+    "title_de": "Aufrüstung",
+    "title_en": "Armament",
+    "type": "Zugkarte",
+    "text": "Eine Figur darf im nächsten Zug schießen, dies bedeutet sie kann eine Figur die sonst auch geschlagen werden kann entfernen ohne sich von dem Platz zu bewegen."
+  },
+  {
+    "cardId": 20,
+    "title_de": "Portal",
+    "title_en": "portal",
+    "type": "Weltkarte",
+    "text": "Bestimme zwei Felder auf dennen keine gegnerischen Figuren sitzen. Eine Figur die auf einem der Felder landet wird auf das andere Feld teleportiert."
+  },
+  {
+    "cardId": 21,
+    "title_de": "Treibsand",
+    "title_en": "Quicksand",
+    "type": "Weltkarte",
+    "text": "Bestimme ein Feld, jede Figur die versucht darüber hinweg zufahren, landet auf diesem Feld. (Springer hüpfen darüber)"
+  },
+  {
+    "cardId": 22,
+    "title_de": "Befestigunswall",
+    "title_en": "Rampart",
+    "type": "Weltkarte",
+    "text": "Setze eine Mauer, welche drei Felder lang ist, zwischen 6 Felder ein. Keine Figur kann darüber hinwegziehen. (Springer hüpfen darüber)"
+  },
+  {
+    "cardId": 23,
+    "title_de": "Selbstmordattentat",
+    "title_en": "suicide assassination",
+    "type": "Zugkarte",
+    "text": "Bestimme eine eigene Figur mit Wertigkeit 3. Nach deinem Zug stirbt diese Figur und falls Figuren ein Feld rechts und links davon stehen, diese auch."
+  },
+  {
+    "cardId": 24,
+    "title_de": "Verlockung",
+    "title_en": "Temptation",
+    "type": "Zugkarte",
+    "text": "Wenn im nächsten Zug geschlagen werden kann, muss man dies tun, außer man opfert stattdesen eine Figur und zieht nicht."
+  },
+  {
+    "cardId": 25,
+    "title_de": "Fusionswelt",
+    "title_en": "Fusionswelt",
+    "type": "Weltkarte",
+    "text": "Anstatt eines Zuges dürfen Figuren gegen eine bessere Figur für den gleichen Wert getauscht werden. An einer Stelle wo die alte Figuren standen wird die Figur eingesetzt, der Gegner bestimmt an welcher."
+  },
+  {
+    "cardId": 26,
+    "title_de": "Erdbeben",
+    "title_en": "Earth Quake",
+    "type": "Zugkarte",
+    "text": "Im nächsten Zug darf man anstatt einen Zug zu machen alle Figuren in einer Reihe um ein Feld nach rechts oder alle um ein Feld nach links versetzen."
+  }
+];
+// liefert ein element anhand der id in einem array
+function getElement(a, id) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i].cardId == id || a[i].id == id) {
+            return a[i];
+        }
+    }
+    alert("Error card not found");
+}
+
+//das karten-objekt
+function card(id) {
+    var cardId = "";
+    var title_de = "";
+    var title_en = "";
+    var type = "";
+    var text = "";
+
+    this.cardId = cardId;
+    this.title_de = title_de;
+    this.title_en = title_en;
+    this.type = type;
+    this.text = text;
+
+    this.setCard = function (_cardId) {
+        var c = getElement(alleKarten,  _cardId);
+        this.cardId = c.cardId;
+        this.title_de = c.title_de;
+        this.title_en = c.title_en;
+        this.type = c.type;
+        this.text = c.text;
+    }
+
+    //wenn mit parameter aufgerufen wird
+    if (id) {
+        this.setCard(id);
+    }
+}
