929e12360ea031c43ee6b2e0d7b0c4e359b883f9
[Antonio] <[antonio.rajic@student.uni-zuebingen.de]>
2020-05-11 04:03:40 +0200

    Brett wird jetzt mit feld-Objekt erstellt

--- js/boardLogic.js
+++ js/boardLogic.js
@@ -1,44 +1,57 @@
 // setzt eine Figur auf das Brett
-// erwartet das feld (id), figur und farbe als parameter
-// toDo: besser wäre erst ein Object der Figur zu haben
-function setFigure(field, figure, color)
+// erwartet das feld als objekt als parameter
+function drawField(field)
 {
-	var feld_=g(field);
-	var fico=figure.toUpperCase().substring(0, 1) + color.toLowerCase().substring(0, 1);
-	feld_.style.backgroundImage="url(\"img/svg/" + fico  + ".svg\")";
-	feld_.style.backgroundSize="cover";
-}
-
-// baut die Ausgangsstellung des Spiels auf
-function setBoard()
-{
-	// zuerst alles löschen
-	clearBoard("table1");
-
-	var aufstellung=["R","N","B","Q","K","B","N","R"];
-	for(var i=1;i<=8;i++)
-	{
-		//bauern
-		setFigure(xr(i)+2, "P", "w");
-		setFigure(xr(i)+7, "P", "b");
-
-		setFigure(xr(i)+1, aufstellung[i-1], "w");
-		setFigure(xr(i)+8, aufstellung[i-1], "b");
-	}
-}
-
-// löscht alle Figuren vom Brett
-function clearBoard(field)
-{
-	var feld_=g(field);
-	var trs=feld_.getElementsByTagName("tr");
-	for(var i=0;i<trs.length;i++)
-    {
-        var tds=trs[i].getElementsByTagName("td");
-        // spalten durch gehen
-    	for(var j=0;j<tds.length;j++)
-    	{
-    		tds[j].style.backgroundImage="";
-    	}
+    var feld_ = g(field.id);
+    // was ist auf dem feld?
+    var s = field.getFigur().length;
+    if (s == 1) {
+        feld_.style.backgroundImage = "url(" + field.getFigur()[0].img + ")";
+        feld_.style.backgroundSize = "cover";
     }
+    if (s == 0) {
+        feld_.style.backgroundImage = "";
+    }
+    if (s > 1) {
+        // toDO
+    }
+}
+
+function setBoard(board) {
+    var res = [];
+    var aufstellung = ["R", "N", "B", "Q", "K", "B", "N", "R"];
+    var trs = board.getElementsByTagName("tr");
+    var table_size = trs.length;
+
+    // zeilen durchgehen
+    for (var i = 0; i < table_size; i++) {
+        var tds = trs[i].getElementsByTagName("td");
+        // spalten durch gehen
+        for (var j = 0; j < table_size; j++) {
+            var td = tds[j];
+            var id = td.getAttribute("id");
+            // feld erzeugen
+            var f = new feld(id);
+            var fi;
+
+            // figuren erzeugen und setzen
+            //bauern
+            if ((f.y == 2 || f.y==7) && f.x > 0 && f.x < 9) {
+                fi = f.y==2 ? new figur("Pw") : new figur("Pb");
+            }
+            // alle anderen figuren
+            if ((f.y == 1 || f.y == 8) && f.x > 0 && f.x < 9) {
+                fi = f.y == 1 ? new figur(aufstellung[f.x - 1] + "w") : new figur(aufstellung[f.x - 1] + "b");
+            }
+
+            if (fi !== undefined) {
+                f.setFigur(fi);
+                fi = undefined;
+            }
+
+            drawField(f);
+            res.push(f);
+        }
+    }
+    return res;
 }
